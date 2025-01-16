5763ef2bdc4f08e96d2216cc4c5a8de836ab9253
Egor Boev <Ego.Boev@x5.ru>
2025-01-17 00:40:18 +0300

    v2 - added test_service_load, docstrings, test class TestDeliveryCost

--- tests/delivery_cost_calculation/test_delivery_cost_function.py
+++ tests/delivery_cost_calculation/test_delivery_cost_function.py
@@ -4,62 +4,82 @@ from delivery_cost_function import delivery_cost_calculation, DeliveryServiceLoa
     FragileObjectDistanceException
 
 
-@pytest.mark.parametrize(
-    ['is_big_volume', 'total'],
-    [
-        [True, 560.0],
-        [False, 420.0]
-    ]
-)
-def test_cargo_volume(is_big_volume, total):
-    assert delivery_cost_calculation(
-        distance=10,
-        is_big_volume=is_big_volume,
-        delivery_service_load=DeliveryServiceLoad.HIGH.value,
-        is_fragile=True
-    ) == total
+class TestDeliveryCost:
+    @pytest.mark.parametrize(
+        ['is_big_volume', 'total'],
+        [
+            [True, 560.0],
+            [False, 420.0]
+        ]
+    )
+    def test_cargo_volume(self, is_big_volume, total):
+        """Проверка с учетом объема груза."""
+        assert delivery_cost_calculation(
+            distance=10,
+            is_big_volume=is_big_volume,
+            delivery_service_load=DeliveryServiceLoad.HIGH.value,
+            is_fragile=True
+        ) == total
 
-
-@pytest.mark.parametrize(
-    ['distance', 'total'],
-    [
-        [0, 400],
-        [2, 420],
-        [10, 560],
-        [30, 700],
-        [400000, 700],
-    ]
-)
-def test_distance(distance, total):
-    assert delivery_cost_calculation(
-        distance=distance,
-        is_big_volume=True,
-        delivery_service_load=DeliveryServiceLoad.HIGH.value,
-        is_fragile=False
-    ) == total
-
-
-@pytest.mark.parametrize(
-    ['is_fragile', 'distance', 'total', 'raises_error'],
-    [
-        [True, 10, 560.0, False],
-        [True, 50, None, True],
-        [False, 50, 700, False]
-    ]
-)
-def test_is_fragile(is_fragile, distance, total, raises_error):
-    if raises_error:
-        with pytest.raises(FragileObjectDistanceException):
-            delivery_cost_calculation(
-                distance=distance,
-                is_big_volume=True,
-                delivery_service_load=DeliveryServiceLoad.HIGH.value,
-                is_fragile=is_fragile
-            )
-    else:
+    @pytest.mark.parametrize(
+        ['distance', 'total'],
+        [
+            [0, 400],
+            [2, 420],
+            [10, 560],
+            [30, 700],
+            [400000, 700],
+        ]
+    )
+    def test_distance(self, distance, total):
+        """Проверка с учетом расстояния до пункта назначения."""
         assert delivery_cost_calculation(
             distance=distance,
             is_big_volume=True,
             delivery_service_load=DeliveryServiceLoad.HIGH.value,
-            is_fragile=is_fragile
+            is_fragile=False
         ) == total
+
+    @pytest.mark.parametrize(
+        ['service_load', 'total'],
+        [
+            [DeliveryServiceLoad.STANDARD.value, 500],
+            [DeliveryServiceLoad.MAJOR.value, 600],
+            [DeliveryServiceLoad.HIGH.value, 700],
+            [DeliveryServiceLoad.VERY_HIGH.value, 800],
+        ]
+    )
+    def test_service_load(self, service_load, total):
+        """Проверка с учетом загруженности службы доставки."""
+        assert delivery_cost_calculation(
+            distance=50,
+            is_big_volume=True,
+            delivery_service_load=service_load,
+            is_fragile=False
+        ) == total
+
+    @pytest.mark.parametrize(
+        ['is_fragile', 'distance', 'total', 'raises_error'],
+        [
+            [True, 10, 560.0, False],
+            [True, 50, None, True],
+            [False, 50, 700, False]
+        ]
+    )
+    def test_is_fragile(self, is_fragile, distance, total, raises_error):
+        """Проверка с учетом хрупкого груза."""
+        if raises_error:
+            with pytest.raises(FragileObjectDistanceException):
+                delivery_cost_calculation(
+                    distance=distance,
+                    is_big_volume=True,
+                    delivery_service_load=DeliveryServiceLoad.HIGH.value,
+                    is_fragile=is_fragile
+                )
+        else:
+            assert delivery_cost_calculation(
+                distance=distance,
+                is_big_volume=True,
+                delivery_service_load=DeliveryServiceLoad.HIGH.value,
+                is_fragile=is_fragile
+            ) == total
